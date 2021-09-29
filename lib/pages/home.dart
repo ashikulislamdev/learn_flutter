f574a7707de69b96123c71d44384858021de8e02
saun2648 <ashikulislamsawan@gmail.com>
2021-09-30 01:58:27 +0600

    Dynamic Data pass via Json

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,17 +1,42 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:learn_app/modals/catalog.dart';
 import 'package:learn_app/widgets/drawer.dart';
 import 'package:learn_app/widgets/item_widget.dart';
 
-class HomaPage extends StatelessWidget {
+class HomaPage extends StatefulWidget {
   const HomaPage({ Key? key }) : super(key: key);
 
+  @override
+  _HomaPageState createState() => _HomaPageState();
+}
+
+class _HomaPageState extends State<HomaPage> {
   final int days = 30;
   final String name = "White";
+
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+  loadData() async {
+    await Future.delayed(Duration(seconds: 2));
+    final catalogJson = await rootBundle.loadString("assets/files/catalog.json");
+    final decodeJson = jsonDecode(catalogJson);
+    var productData = decodeJson["products"];
+    CatalogModal.items = List.from(productData).map<Item>((item) => Item.fromMap(item)).toList();
+    setState(() { });
+  }
+  
+
   @override
   Widget build(BuildContext context) {
 
-  final dummylist = List.generate(25, (index) => CatalogModal.items[0]);
+  //final dummylist = List.generate(25, (index) => CatalogModal.items[0]);
 
     return Scaffold(
       appBar: AppBar(
@@ -23,14 +48,14 @@ class HomaPage extends StatelessWidget {
 
       body: Padding(
         padding: EdgeInsets.all(16.0),
-        child: ListView.builder(
-          itemCount: dummylist.length,
+        child: /* (CatalogModal.items !=null && CatalogModal.items.isNotEmpty)? */ ListView.builder(
+          itemCount: CatalogModal.items.length,
           itemBuilder: (context, index){
             return ItemWidget(
-              item: dummylist[index],
+              item: CatalogModal.items[index],
             );
           },
-        ),
+        ) /* : Center(child: CircularProgressIndicator(),) */,
       ),
     );
   }
